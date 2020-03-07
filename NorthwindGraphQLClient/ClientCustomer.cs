e617b1c141fa724ced9220223d4612628b9bd31a
Erik Dahl <dahlsailrunner@yahoo.com>
2020-03-07 09:34:23 -0600

    Added graphql client

---
+++ NorthwindGraphQLClient/ClientCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace NorthwindGraphQLClient
+{
+    public class CustomerResponse
+    {
+        public List<ClientCustomer> Customers { get; set; }
+    }
+    public class ClientCustomer
+    {
+        public string CustomerId { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+    }
+}
---
+++ NorthwindGraphQLClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using GraphQL.Client.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Client.Serializer.Newtonsoft;
+
+namespace NorthwindGraphQLClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var options = new GraphQLHttpClientOptions
+            {
+                UseWebSocketForQueriesAndMutations = false,
+                EndPoint = new System.Uri("https://localhost:5001/graphql"),
+                JsonSerializer = new NewtonsoftJsonSerializer()
+            };
+            var client = new GraphQLHttpClient(options);            
+            var request = new GraphQLHttpRequest
+            {
+                Query = @"
+                query northwindQuery {
+                      customers {
+                        customerId, 
+                        companyName,
+                        contactName
+                      }
+                }"
+            };
+
+            var response = await client.SendQueryAsync<CustomerResponse>(request);
+            var x = response.Data.Customers.FirstOrDefault();
+        }
+    }
+}
